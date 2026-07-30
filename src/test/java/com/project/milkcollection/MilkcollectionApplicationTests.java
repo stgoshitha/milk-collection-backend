9d4fd979603882ff3b7f45fde53698f074145a0e
stgoshitha <wkgoshithapremachandra@gmail.com>
2026-07-30 15:55:57 +0530

    chore: initial project setup

---
+++ src/test/java/com/project/milkcollection/MilkcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.milkcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkcollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
