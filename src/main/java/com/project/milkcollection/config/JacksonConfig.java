185d9d1489cc1d798cef51cbd0b67843edef2477
stgoshitha <wkgoshithapremachandra@gmail.com>
2026-08-06 11:07:17 +0530

    fix(config): configure Jackson for LocalDateTime serialization

--- src/main/java/com/project/milkcollection/config/JacksonConfig.java
+++ src/main/java/com/project/milkcollection/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.project.milkcollection.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,11 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.registerModule(new JavaTimeModule());
+
+        return mapper;
     }
 }
